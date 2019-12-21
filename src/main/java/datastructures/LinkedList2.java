46f8cc5edc82368c0afccfbf6fbbc79bdd8a5c69
Waseef Akhtar <waseef@degoo.com>
2019-12-22 01:58:15 +0500

    Boilerplate code for LinkedList2

---
+++ src/main/java/datastructures/LinkedList2.java
@@ -0,0 +1,19 @@
+package datastructures;
+
+public class LinkedList2 {
+
+    private Node first;
+    private Node last;
+
+    public class Node {
+        private int value;
+        private Node next;
+    }
+
+    // addFirst
+    // addLast
+    // deleteFirst
+    // deleteLast
+    // contains
+    // indexOf
+}
